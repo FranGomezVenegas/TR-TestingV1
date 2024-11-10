b2520f6ff8add667d4beb778cc2105c5951ae9e9
Paula <paularomeragarzon@gmail.com>
2024-11-10 17:40:50 +0100

    Creation of the common print test.

--- trazit-models/test-config-instruments-newInstrument.js
+++ trazit-models/test-config-instruments-newInstrument.js
@@ -1,33 +1,11 @@
 export const buttonWithDialog={
-    "buttonName": "create_new_folder",
-	"fldName":{"label":"* New Instrument Name", "value":"prueba{TZ_DATE}"}, 
-    "fldFamily":{"label":"* Family", "option":"Viscosimetro"},
-    "fldModel":{"label":"* Model", "value":"123"},
-    "fldSupplier":{"label":"* Supplier", "option":"PB Instruments"},
-    "fldSerial":{"label":"* Serial Number", "value":"456"},
-    "fldManufacturer":{"label":"* ManufacturerName", "option":"Mettler Toledo"},
-    "fldResponsible":{"label":"Responsible", "option":"admin"},
-    "fldResponsibleBackup":{"label":"Responsible Backup", "option":"admin"},
-    "fldPurchase":{"label":"Purchase Date", "value":"2024-09-01"},
-    "fldInstallation":{"label":"* Installation Date", "value":"2024-09-01"},
-    "buttonAccept": "Accept",
-    "buttonCancel": "Cancel",
-
-
-    "screenShotsEmptyForm": "Form Empty",
-    "screenShotsFilledForm": "Form Filled",
-    "screenShotsAccept": "Accept",
-    "screenShotsCancel": "Cancel",
-    
-    "textInNotif1": "Instrument",
-    "textInNotif2": "created",
-    "textInNotif3": "success",
-
-    "endpoints":{
-        "Queries":[""],
-        "endpointsActions":["NEW_INSTRUMENT"]
-    },
+    "testDataGame": "{\"procInstanceName\":\"mb_em\",\"desktopMode\":{\"local\":true,\"pageElementName\":\"sp-action-menu#dashboardprocedures\",\"screenShotsName\":\"window open procedure\",\"label\":\"Micro EM\",\"view\":\"Air\",\"viewScreenShotLabel\":\"Air\",\"pageElement\":\"dashboard/procedures?procName=mb_em&viewName=SamplePendingSampling&filterName=LOCATION\",\"screenShotName\":\"Pending Sampling Location\"},\"mobileMode\":{\"pageElementName\":\"md-list-item#dashboardmyprocedures\",\"clickMenu\":\"md-filled-icon-button.menu\",\"screenShotsName\":\"Opening the Procedure Menu\",\"label\":\".mb_em\",\"viewScreenShotLabel\":\"Air\",\"pageElement\":\"dashboard/procedures?procName=mb_em&viewName=SamplePendingSampling&filterName=LOCATION\",\"screenShotName\":\"Pending Sampling Location\"},\"tabletRetratoMode\":{\"pageElementName\":\"sp-action-menu#dashboardprocedures\",\"screenShotsName\":\"window open procedure\",\"label\":\"Micro EM\",\"view\":\"Air\",\"viewScreenShotLabel\":\"Air\",\"pageElement\":\"dashboard/procedures?procName=mb_em&viewName=SamplePendingSampling&filterName=LOCATION\",\"screenShotName\":\"Pending Sampling Location\"},\"buttonName\":\"SAMPLINGCOMMENTADD\",\"textInNotif1\":\"comment\",\"textInNotif2\":\"added\",\"textInNotif3\":\"properly\",\"phrasePauses\":\"Execution Pauses\",\"phraseScreenShots\":\"Attach ScreenShots\",\"screenShotsButtonName\":\"Click a Button\",\"text1\":{\"label\":\"new Comment\",\"value\":\"testing\"},\"screenShotsEmptyForm\":\"Empty Form\",\"screenShotsFilledForm\":\"Filled Form\",\"selectName\":\"80\",\"phraseSelect\":\"Select Element\",\"screenShotsElement\":\"Select Element\"}"
+  }
+  
+export const print={
+    "testDataGame": "{\"desktopMode\":{\"local\":true,\"pageElementName\":\"sp-action-menu#dashboardprocedures\",\"screenShotsName\":\"window open procedure\",\"label\":\"Water Monitoring\",\"view\":\"Water\",\"viewScreenShotLabel\":\"Water\",\"pageElement\":\"dashboard/procedures?procName=mon_water&viewName=ProductionLots&filterName=\",\"screenShotName\":\"Pending Sampling\"},\"mobileMode\":{\"pageElementName\":\"md-list-item#dashboardmyprocedures\",\"clickMenu\":\"md-filled-icon-button.menu\",\"screenShotsName\":\"Opening the Procedure Menu\",\"label\":\".mon_water\",\"viewScreenShotLabel\":\"Water\",\"pageElement\":\"dashboard/procedures?procName=mon_water&viewName=ProductionLots&filterName=\",\"screenShotName\":\"Pending Sampling\"},\"tabletRetratoMode\":{\"pageElementName\":\"sp-action-menu#dashboardprocedures\",\"screenShotsName\":\"window open procedure\",\"label\":\"Water Monitoring\",\"view\":\"Water\",\"viewScreenShotLabel\":\"Water\",\"pageElement\":\"dashboard/procedures?procName=mon_water&viewName=ProductionLots&filterName=\",\"screenShotName\":\"Pending Sampling\"},\"phrasePrintDialog\":\"Open print dialog in new tab\",\"phraseWaitForPrintDialog\":\"Wait for print dialog page to load\",\"phrasePdfSavePath\":\"Prepare the PDF saving path\",\"phraseGeneratePdfWithOrientation\":\"Generate PDF in selected orientation\",\"phrasePdfAttachment\":\"Attach the generated PDF to the test report\",\"phraseClosePrintDialog\":\"Close the print dialog page\",\"buttonName\":\"print\",\"phrasePauses\":\"Execution Pauses\",\"phraseScreenShots\":\"Attach ScreenShots\",\"screenShotsButtonName\":\"Click a Button\", \"basePath\": \"./print\",\"orientation\":\"horizontal\"}"  
 }
+
 
 export const buttonDownload={
     "buttonName": "create_new_folder",
