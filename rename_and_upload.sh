63ddf4240c64ec2cc46a3cd4545c31e07eb1ce88
Paula <paularomeragarzon@gmail.com>
2024-12-03 12:22:05 +0100

    Adjustment of the rename_and_upload.sh script to delete the local folder if the TRAZIT_TEST_NAME already exists.

--- rename_and_upload.sh
+++ rename_and_upload.sh
@@ -11,7 +11,7 @@ AWS_BUCKET="$1"
 TRAZIT_TEST_NAME="$2"
 PLAYWRIGHT_FILENAME="$3"
 PLAYWRIGHT_TESTNAME="$4"
-PROC_INSTANCE_NAME="$5"  # Recibiendo el procInstanceName como el quinto parámetro
+PROC_INSTANCE_NAME="$5"
 
 # Definir el perfil y el directorio de reportes
 profile='default'
@@ -19,7 +19,6 @@ playwright_reports_dir='playwright_reports'
 
 # Definir los directorios base y nuevo nombre
 REPORTS_DIR="./htmlreport"
-
 CURRENT_FILE_NAME="filename"
 CURRENT_TEST_NAME="testname"
 
@@ -40,7 +39,9 @@ echo "Current File Name: $CURRENT_FILE_NAME"
 echo "Current Test Name: $CURRENT_TEST_NAME"
 echo "New File Name: $TRAZIT_TEST_NAME"
 echo "New Test Name: $PLAYWRIGHT_TESTNAME"
-echo "Proc Instance Name: $PROC_INSTANCE_NAME"  # Mostrar procInstanceName
+echo "Proc Instance Name: $PROC_INSTANCE_NAME"
+
+# Mostrar rutas
 echo "Rutas actuales:"
 echo "Current Outer Directory: $CURRENT_OUTER_DIR"
 echo "Current Inner Directory: $CURRENT_INNER_DIR"
@@ -51,27 +52,31 @@ echo "New Inner Directory: $NEW_INNER_DIR"
 # Verificar si el directorio interior actual existe
 if [ -d "$CURRENT_INNER_DIR" ]; then
     echo "El directorio interior existe: ${CURRENT_INNER_DIR}"
-
-    echo "Renombrando directorios..."
-
-    # Crear el nuevo directorio exterior si no existe
-    if [ ! -d "$NEW_OUTER_DIR" ]; then
-        mkdir -p "$NEW_OUTER_DIR"
+    
+    # Eliminar el directorio nuevo si ya existe
+    if [ -d "$NEW_OUTER_DIR" ]; then
+        echo "Eliminando directorio existente: $NEW_OUTER_DIR"
+        rm -rf "$NEW_OUTER_DIR"
     fi
 
+    echo "Renombrando directorios..."
+    
+    # Crear el nuevo directorio exterior
+    mkdir -p "$NEW_OUTER_DIR"
+    
     # Mover el directorio interior a la nueva ubicación
     mv "$CURRENT_INNER_DIR" "$NEW_INNER_DIR"
     echo "Carpeta interior renombrada y movida: $CURRENT_INNER_DIR -> $NEW_INNER_DIR"
-
+    
     # Mover el directorio exterior a la nueva ubicación
     rmdir "$CURRENT_OUTER_DIR"
     mv "$NEW_INNER_DIR" "$NEW_OUTER_DIR/$PLAYWRIGHT_TESTNAME"
     echo "Carpeta exterior renombrada: $CURRENT_OUTER_DIR -> $NEW_OUTER_DIR"
-
+    
     # Subir a AWS
     echo "Subiendo el reporte a AWS Bucket: $AWS_BUCKET"
     
-    # Borrar la carpeta existente en S3 que tenga el mismo nombre
+    # Borrar la carpeta existente en S3
     echo "Eliminando carpeta existente en S3 si la hay..."
     aws --profile $profile s3 rm s3://$AWS_BUCKET/$playwright_reports_dir/$TRAZIT_TEST_NAME --recursive
     
