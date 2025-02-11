69febe3c11db473237935ba8f2643d5dea24f147
Paula <paularomeragarzon@gmail.com>
2025-02-11 20:15:20 +0100

    new functionality on the 'Object by Tabs' screen to interact with the list of objects in the left panel, allowing users to either type the object or select an object from the list and then perform the search.

--- tests/1TRAZiT-Commons/objectByTabsWithSearch.js
+++ tests/1TRAZiT-Commons/objectByTabsWithSearch.js
@@ -6,6 +6,7 @@ export const clickElementWithWildcard = async (page, selector, useWildcard = fal
     const modifiedSelector = useWildcard ? `* ${selector}` : selector;
     await clickElement(page, modifiedSelector, timeout);
 };
+
 export const fillFieldWithWildcard = async (page, selector, value, useWildcard = false) => {
     const modifiedSelector = useWildcard ? `* ${selector}` : selector;
     await fillField(page, modifiedSelector, value);
@@ -14,33 +15,33 @@ export const fillFieldWithWildcard = async (page, selector, value, useWildcard =
 // Ejemplo de uso en handleObjectByTabsWithSearchInteraction
 export async function handleObjectByTabsWithSearchInteraction(page, testInfo, configSettings, button) {
     if (button.search) {
-        console.log('Se encontró el buscador...'); 
+        console.log('Se encontró el buscador...');
         await test.step(button.phraseClickSearch, async () => {
-            // Utiliza clickElementWithWildcard con el parámetro adicional para agregar el comodín si es necesario
-            await clickElementWithWildcard(page, button.search.label, true); // useWildcard en true
-            console.log('Hizo clic en el buscador...'); 
-        });
-        
-        await test.step(button.phrasePauses, async () => {
-            await page.pause();
+            // Unificamos la condición: si `label` o `value` están vacíos o no definidos, hacemos clic en en la opcion 
+            if (button.search.label && button.search.value) {
+                // Si ambos `label` y `value` están presentes y son válidos, hacer clic y llenar el campo
+                await clickElementWithWildcard(page, button.search.label, true); // useWildcard en true
+                console.log('Hizo clic en el buscador...');
+                
+                await fillFieldWithWildcard(page, button.search.label, button.search.value, true); // useWildcard en true
+                console.log('Escribiendo en el buscador...');
+            } else {
+                // Si `label` o `value` no están definidos, hacer clic en el botón en la opcion 
+                console.log('No se encontraron label o value. Haciendo clic en el botón');
+                await page.getByRole('button', { name: button.search.selectObjectSearch }).click({ force: true, timeout: 2000 });
+            }
         });
 
-        await test.step(button.phraseFillField, async () => { 
-            // Uso de fillFieldWithWildcard para escribir en el campo con la opción de comodín activada
-            await fillFieldWithWildcard(page, button.search.label, button.search.value, true); // useWildcard en true
-            console.log('Escribiendo en el buscador...'); 
-        });
-        
         await test.step(button.phrasePauses, async () => {
             await page.pause();
         });
 
         await test.step(button.phraseSearch, async () => {
-            try{
-                await page.getByRole('button', { name: button.search.press }).click({timeout: 1000});
+            try {
+                await page.getByRole('button', { name: button.search.press }).click({ timeout: 1000 });
                 console.log('Búsqueda realizada.');
-            } catch(error){
-                await page.getByText(button.search.press, {exact: true}).click({force: true, timeout: 1000});
+            } catch (error) {
+                await page.getByText(button.search.press, { exact: true }).click({ force: true, timeout: 1000 });
                 console.log('Búsqueda Realizada.');
             }
         });
@@ -50,9 +51,6 @@ export async function handleObjectByTabsWithSearchInteraction(page, testInfo, co
             await page.pause();
             await page.pause();
             await page.pause();
-            await test.step('Esperar 3 segundos para que la página cargue completamente.', async () => {
-                await page.waitForTimeout(3000); 
-            }); 
         });
 
         await test.step(button.phraseScreenShots, async () => {
