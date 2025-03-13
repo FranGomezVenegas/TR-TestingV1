f18cdb18f5aefae3e9c557e7f42c0be85b33fda0
Paula <paularomeragarzon@gmail.com>
2025-03-13 19:41:04 +0100

    Modified the test to drag a sample to an incubator, making the drag movement smoother, and added a test.step.

--- tests/Common/AddSampleToAnIncubation.spec.ts
+++ tests/Common/AddSampleToAnIncubation.spec.ts
@@ -19,9 +19,171 @@ import {handleActionNameInteraction} from '../1TRAZiT-Commons/actionsNameInterac
 import { handleMenus } from '../1TRAZiT-Commons/handleMenus';
 
 
-const commonTests = async (ConfigSettings: any, page: any, testInfo: any) => {
-    await handleMenus(page);
+// const commonTests = async (ConfigSettings: any, page: any, testInfo: any) => {
+//     await handleMenus(page);
 
+//     // Create instances of Logger and NetworkInterceptor
+//     const logger = new Logger();
+//     const networkInterceptor = new NetworkInterceptor();
+
+//     let Button;
+
+//     // If configuration data is available, process the JSON
+//     if (ConfigSettings && ConfigSettings.dataForTest) {
+//         let unescapedString = ConfigSettings.dataForTest.replace(/\\+/g, '\\');
+//         try {
+//             Button = JSON.parse(unescapedString);
+//         } catch (error) {
+//             console.error("Error parsing JSON:", error);
+//         }
+//         Button = JSON.parse(Button.testDataGame);
+//     } else {
+//         Button = dataForTestFromFile;
+//     }
+
+//     // Attach Logger and NetworkInterceptor to the page
+//     await test.step(phraseReport.phraseNetworkInterceptionAndLogger, async () => {
+//         logger.attachToPage(page);
+//         networkInterceptor.attachToPage(page);
+//     });
+
+//     // NOTIFICATIONS
+//     let afterEachData = {
+//         textInNotif1: Button.textInNotif1,
+//         textInNotif2: Button.textInNotif2,
+//         textInNotif3: Button.textInNotif3,
+//     };
+
+//     const notificationWitness = new NotificationWitness(page);
+
+//     // selecciono el elemento 
+//     await test.step(Button.phraseSelect, async () => {
+//         await page.getByRole('cell', { name: Button.selectName, exact: true }).click({timeout: 5000});
+
+//         await test.step(Button.phraseScreenShots, async () => {
+//             await attachScreenshot(testInfo, Button.screenShotsSelect, page, ConfigSettingsAlternative.screenShotsContentType);
+//             if (Button.phrasePauses) {
+//                 await page.pause();
+//             }
+//         });
+//     });
+    
+
+//     let selectElement: any;
+
+//     // Paso 1: Encontrar el elemento y hacer scroll hacia él
+//     await test.step(Button.phraseElementToMove, async () => {
+//         selectElement = await page.getByRole('cell', { name: Button.selectElementToMove, exact: true });
+//         await selectElement.scrollIntoViewIfNeeded();
+//         await page.waitForTimeout(500);
+
+//         await test.step(Button.phraseScreenShots, async () => {
+//             await attachScreenshot(testInfo, Button.screenShotsElementToMove, page, ConfigSettingsAlternative.screenShotsContentType);
+//             if (Button.phrasePauses) {
+//                 await page.pause();
+//             }
+//         });
+//     });
+
+//     // Paso 2: Variable para la posicion del elemento
+//     let positionElement: { x: number; y: number; width: number; height: number } | null = null;
+
+//     // Paso 3: Obtengo la posición inicial del elemento seleccionado.
+//     await test.step(Button.phraseInitialPosition, async () => {
+//         const boundingBox = await selectElement.boundingBox();
+//         console.log('Elemento a mover:', await selectElement.textContent());
+//         console.log('Posición del elemento antes de mover:', boundingBox);
+//         positionElement = boundingBox;
+//     });
+
+//     // Paso 4: Realizar el drag and drop
+//   await test.step(Button.phraseDrapAndDrop, async () => {
+//     const mainBox = await page.locator(Button.container);
+//     const posicionMainBox = await mainBox.boundingBox();
+
+//     if (positionElement && posicionMainBox) {
+//       // Mueve el ratón a la posición central de elemento seleccionado
+//       await page.mouse.move(
+//         positionElement.x + positionElement.width / 2,
+//         positionElement.y + positionElement.height / 2
+//       );
+//       await page.mouse.down();
+//       console.log('Iniciando arrastre del elemento 132');
+
+//       await test.step(Button.phraseScreenShots, async () => {
+//         await attachScreenshot(testInfo, Button.screenShotsDrapStarted, page, ConfigSettingsAlternative.screenShotsContentType);
+//         if (Button.phrasePauses) {
+//             await page.pause();
+//         }
+//     });
+
+//       const steps = 50; // Aumentar el número de pasos para una animación más suave
+//       for (let i = 0; i <= steps; i++) {
+//         const x = posicionMainBox.x + posicionMainBox.width / 2;
+//         const y = posicionMainBox.y + posicionMainBox.height / 2;
+//         await page.mouse.move(x, y, { steps: 20 }); // Mover el ratón a la posición central del mainBox
+//         await page.waitForTimeout(30); // Reducir el tiempo de espera entre pasos
+//       }
+
+//       await page.mouse.up();
+//       console.log('Arrastre completado para el elemento');
+//       await page.waitForTimeout(2000);
+//       await test.step(Button.phraseScreenShots, async () => {
+//         await attachScreenshot(testInfo, Button.screenShotsDraggingOfTheElement, page, ConfigSettingsAlternative.screenShotsContentType);
+//             if (Button.phrasePauses) {
+//                 await page.pause();
+//             }
+//         });
+//     }
+//   });
+
+//   // Justification Phrase
+//   await fillUserField(page, testInfo); // Rellena el campo de "User"
+//   await fillPasswordField(page, testInfo); // Rellena el campo de "Password"
+
+//   // Continuar con la justificación y otras acciones
+//   await justificationPhrase(page, 30000, testInfo);  
+//   await esignRequired(page, 30000, testInfo);
+
+//   await clickAcceptButton(page);
+//   await clickDoButton(page);
+  
+//   // Verificar que no haya errores en la consola
+//   await test.step(phraseReport.phraseError, async () => {
+//     logger.printLogs();
+//     expect(logger.errors.length).toBe(0);
+//   });
+
+//   // Verificar respuestas de red capturadas
+//   await test.step(phraseReport.phraseVerifyNetwork, async () => {
+//       networkInterceptor.printNetworkData();
+//       const nullResponsesCount = networkInterceptor.verifyNonImageNullResponses();
+//       expect(nullResponsesCount).toBe(0);  // Asegúrate de que no haya respuestas nulas
+//   });
+
+//   // Validar respuestas utilizando ResponseValidator
+//   await test.step(phraseReport.phraseVerifyNetwork, async () => {
+//       const responseValidator = new ResponseValidator(networkInterceptor.responses);
+//       try {
+//           await responseValidator.validateResponses(); // Lanza un error si no hay respuestas válidas
+//       } catch (error) {
+//           // test.fail(error.message); // Marca la prueba como fallida con el mensaje
+//       }
+//   });
+
+//   const mode = await notificationWitness.getDeviceMode(testInfo);
+
+//   // Llamar a addNotificationWitness después de realizar acciones
+//   await test.step(ReportNotificationPhase.phraseCaptureNotification, async () => {
+//       const capturedObjectName = await notificationWitness.addNotificationWitness(testInfo, afterEachData, mode);
+//       console.log('Captured Object Name:', capturedObjectName);
+//   });
+// }
+
+
+// Test suite setup for Desktop Mode
+
+const commonTests = async (ConfigSettings: any, page: any, testInfo: any) => {
     // Create instances of Logger and NetworkInterceptor
     const logger = new Logger();
     const networkInterceptor = new NetworkInterceptor();
@@ -96,92 +258,83 @@ const commonTests = async (ConfigSettings: any, page: any, testInfo: any) => {
         positionElement = boundingBox;
     });
 
-    // Paso 4: Realizar el drag and drop
-  await test.step(Button.phraseDrapAndDrop, async () => {
-    const mainBox = await page.locator(Button.container);
-    const posicionMainBox = await mainBox.boundingBox();
+    // Paso 4: Realizar el drag and drop usando dragTo con movimiento lento
+    await test.step(Button.phraseDrapAndDrop, async () => {
+        const mainBox = await page.locator(Button.container);
+        const posicionMainBox = await mainBox.boundingBox();
 
-    if (positionElement && posicionMainBox) {
-      // Mueve el ratón a la posición central de elemento seleccionado
-      await page.mouse.move(
-        positionElement.x + positionElement.width / 2,
-        positionElement.y + positionElement.height / 2
-      );
-      await page.mouse.down();
-      console.log('Iniciando arrastre del elemento 132');
+        if (positionElement && posicionMainBox) {
+            // Usar la función dragTo con un número alto de pasos para un movimiento más lento
+            await selectElement.dragTo(mainBox, {
+                sourcePosition: {
+                    x: positionElement.width / 2,
+                    y: positionElement.height / 2
+                },
+                targetPosition: {
+                    x: posicionMainBox.width / 2,
+                    y: posicionMainBox.height / 2
+                },
+                steps: 250 // Aumentar el número de pasos para un movimiento más lento y que sea visible el movimiento.
+            });
 
-      await test.step(Button.phraseScreenShots, async () => {
-        await attachScreenshot(testInfo, Button.screenShotsDrapStarted, page, ConfigSettingsAlternative.screenShotsContentType);
-        if (Button.phrasePauses) {
-            await page.pause();
+            console.log('Arrastre completado para el elemento');
+            await page.waitForTimeout(2000);
+
+            await test.step(Button.phraseScreenShots, async () => {
+                await attachScreenshot(testInfo, Button.screenShotsDraggingOfTheElement, page, ConfigSettingsAlternative.screenShotsContentType);
+                if (Button.phrasePauses) {
+                    await page.pause();
+                }
+            });
         }
     });
 
-      const steps = 50; // Aumentar el número de pasos para una animación más suave
-      for (let i = 0; i <= steps; i++) {
-        const x = posicionMainBox.x + posicionMainBox.width / 2;
-        const y = posicionMainBox.y + posicionMainBox.height / 2;
-        await page.mouse.move(x, y, { steps: 20 }); // Mover el ratón a la posición central del mainBox
-        await page.waitForTimeout(30); // Reducir el tiempo de espera entre pasos
-      }
+    await test.step("Final checks", async () => {
+        // Justification Phrase
+        await fillUserField(page, testInfo); // Rellena el campo de "User"
+        await fillPasswordField(page, testInfo); // Rellena el campo de "Password"
 
-      await page.mouse.up();
-      console.log('Arrastre completado para el elemento');
-      await page.waitForTimeout(2000);
-      await test.step(Button.phraseScreenShots, async () => {
-        await attachScreenshot(testInfo, Button.screenShotsDraggingOfTheElement, page, ConfigSettingsAlternative.screenShotsContentType);
-            if (Button.phrasePauses) {
-                await page.pause();
-            }
-        });
-    }
-  });
+        // Continuar con la justificación y otras acciones
+        await justificationPhrase(page, 30000, testInfo);  
+        await esignRequired(page, 30000, testInfo);
 
-  // Justification Phrase
-  await fillUserField(page, testInfo); // Rellena el campo de "User"
-  await fillPasswordField(page, testInfo); // Rellena el campo de "Password"
+        await clickAcceptButton(page);
+        await clickDoButton(page);
+    });
+    
+    // Verificar que no haya errores en la consola
+    await test.step(phraseReport.phraseError, async () => {
+        logger.printLogs();
+        expect(logger.errors.length).toBe(0);
+    });
 
-  // Continuar con la justificación y otras acciones
-  await justificationPhrase(page, 30000, testInfo);  
-  await esignRequired(page, 30000, testInfo);
+    // Verificar respuestas de red capturadas
+    await test.step(phraseReport.phraseVerifyNetwork, async () => {
+        networkInterceptor.printNetworkData();
+        const nullResponsesCount = networkInterceptor.verifyNonImageNullResponses();
+        expect(nullResponsesCount).toBe(0);  // Asegúrate de que no haya respuestas nulas
+    });
 
-  await clickAcceptButton(page);
-  await clickDoButton(page);
-  
-  // Verificar que no haya errores en la consola
-  await test.step(phraseReport.phraseError, async () => {
-    logger.printLogs();
-    expect(logger.errors.length).toBe(0);
-  });
+    // Validar respuestas utilizando ResponseValidator
+    await test.step(phraseReport.phraseVerifyNetwork, async () => {
+        const responseValidator = new ResponseValidator(networkInterceptor.responses);
+        try {
+            await responseValidator.validateResponses(); // Lanza un error si no hay respuestas válidas
+        } catch (error) {
+            // test.fail(error.message); // Marca la prueba como fallida con el mensaje
+        }
+    });
 
-  // Verificar respuestas de red capturadas
-  await test.step(phraseReport.phraseVerifyNetwork, async () => {
-      networkInterceptor.printNetworkData();
-      const nullResponsesCount = networkInterceptor.verifyNonImageNullResponses();
-      expect(nullResponsesCount).toBe(0);  // Asegúrate de que no haya respuestas nulas
-  });
+    const mode = await notificationWitness.getDeviceMode(testInfo);
 
-  // Validar respuestas utilizando ResponseValidator
-  await test.step(phraseReport.phraseVerifyNetwork, async () => {
-      const responseValidator = new ResponseValidator(networkInterceptor.responses);
-      try {
-          await responseValidator.validateResponses(); // Lanza un error si no hay respuestas válidas
-      } catch (error) {
-          // test.fail(error.message); // Marca la prueba como fallida con el mensaje
-      }
-  });
-
-  const mode = await notificationWitness.getDeviceMode(testInfo);
-
-  // Llamar a addNotificationWitness después de realizar acciones
-  await test.step(ReportNotificationPhase.phraseCaptureNotification, async () => {
-      const capturedObjectName = await notificationWitness.addNotificationWitness(testInfo, afterEachData, mode);
-      console.log('Captured Object Name:', capturedObjectName);
-  });
+    // Llamar a addNotificationWitness después de realizar acciones
+    await test.step(ReportNotificationPhase.phraseCaptureNotification, async () => {
+        const capturedObjectName = await notificationWitness.addNotificationWitness(testInfo, afterEachData, mode);
+        console.log('Captured Object Name:', capturedObjectName);
+    });
 }
 
 
-// Test suite setup for Desktop Mode
 let trazitTestName;
 let procInstanceName;
 let ConfigSettings;
